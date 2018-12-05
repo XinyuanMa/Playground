c22f373bfe47bc9f81fee984cc1b7046f82c9d5a
Allen <xinyuan@landing.ai>
2018-12-04 19:03:38 -0800

    first batch of interface

--- src/main/java/allen/ma/Storage/PictureStorage.java
+++ src/main/java/allen/ma/Storage/PictureStorage.java
@@ -1,7 +1,18 @@
 package allen.ma.Storage;
 
+import allen.ma.Picture;
+
+import java.io.File;
+import java.nio.file.Path;
 import java.util.UUID;
 
 public interface PictureStorage {
 
+  void storePicture(UUID id);
+
+  Picture loadPicture(UUID id);
+  Picture loadPictureFromFolder(File srcFile);
+
+  Path getPicturePath(UUID id);
+  Path getImageMetaFilePath(UUID id);
 }
--- src/main/java/allen/ma/Storage/PictureStorageImpl.java
+++ src/main/java/allen/ma/Storage/PictureStorageImpl.java
@@ -1,7 +1,36 @@
 package allen.ma.Storage;
 
+import allen.ma.Picture;
 import com.google.gson.Gson;
 
-public class PictureStorageImpl {
+import java.io.File;
+import java.nio.file.Path;
+import java.util.UUID;
 
+public class PictureStorageImpl implements PictureStorage {
+
+  @Override
+  public void storePicture(UUID id) {
+
+  }
+
+  @Override
+  public Picture loadPicture(UUID id) {
+    return null;
+  }
+
+  @Override
+  public Picture loadPictureFromFolder(File srcFile) {
+    return null;
+  }
+
+  @Override
+  public Path getPicturePath(UUID id) {
+    return null;
+  }
+
+  @Override
+  public Path getImageMetaFilePath(UUID id) {
+    return null;
+  }
 }
