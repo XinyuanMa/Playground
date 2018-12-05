4d4da2cd92dfbb8082137e8bade25fa7ee4145e9
Allen <xinyuan@landing.ai>
2018-12-05 10:54:17 -0800

    play around git pull --rebase instead of git merge

--- src/main/java/allen/ma/Storage/PictureStorageImpl.java
+++ src/main/java/allen/ma/Storage/PictureStorageImpl.java
@@ -2,12 +2,16 @@ package allen.ma.Storage;
 
 import allen.ma.Picture;
 import com.google.gson.Gson;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.nio.file.Path;
 import java.util.UUID;
 
 public class PictureStorageImpl implements PictureStorage {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(PictureStorageImpl.class);
 
   @Override
   public void storePicture(UUID id) {
